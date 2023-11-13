9505677124962fe6337d8ae9aa0da3d0bef2b7a5
Sergey Ruppel <sergeyruppel@gmail.com>
2023-11-13 19:25:49 +0700

    Project created

---
+++ BMI Calculator/BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Sergey Ruppel on 13.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
