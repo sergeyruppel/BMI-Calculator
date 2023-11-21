f719312ffb6de30a120184c7d20ec4cf5af43caa
Sergey Ruppel <sergeyruppel@users.noreply.github.com>
2023-11-21 21:21:47 +0700

    ResultViewController added

---
+++ BMI Calculator/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Sergey Ruppel on 21.11.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
